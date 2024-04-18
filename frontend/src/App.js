a2ccbafb32a1221e50b2e555a76e618eea7ead14
Eoin Connolly <eoin.connolly.2022@mumail.ie>
2024-04-18 14:28:20 +0100

    Added NavBar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,13 +8,14 @@ import Tasks from './pages/Tasks';
 import BookDesk from './pages/BookDesk';
 import BookMeeting from './pages/BookMeeting';
 import FindDesk from './pages/FindDesk';
+import { NavBar } from './components/NavBar';
 
 
 function App() {
   return (
     <Router>
       <div className = "App">
-        <Header />
+        <NavBar/>
         <Routes>
           <Route path = "/" element = {<Home />} />
           <Route path = "/carpool" element = {<Carpool />} />
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useRef } from 'react';
+import {
+    TabContainer,
+    Tab,
+    TabSeparator
+    } from '@ui5/webcomponents-react';
+
+export function NavBar() {
+    const tabContainerRef = useRef(null);
+
+    useEffect(() => {
+        const ro = new ResizeObserver(entries => {
+            window.requestAnimationFrame(() => {
+                for (let entry of entries) {
+                    const {width, height} = entry.contentRect;
+                    console.log('Element:', entry.target);
+                    console.log(`Element size: ${width}px x ${height}px`);
+                    console.log(`Element padding: ${entry.target.style.padding}`);
+                }
+            });
+        });
+
+        if (tabContainerRef.current) {
+            ro.observe(tabContainerRef.current);
+        }
+
+        return () => {
+            if (tabContainerRef.current) {
+                ro.unobserve(tabContainerRef.current);
+            }
+        };
+    }, []);
+    return(
+        <TabContainer
+        contentBackgroundDesign="Solid"
+        headerBackgroundDesign="Solid"
+        //onTabSelect={function _a(){}}
+        tabLayout="Standard"
+        >
+        <Tab
+            selected
+            subTabs={<><Tab text="Tab 1.1" /><Tab text="Tab 1.2" /><Tab text="Tab 1.3" /></>}
+            text="Home"
+        >
+            <div
+            style={{
+                display: 'none'
+            }}
+            />
+        </Tab>
+        <TabSeparator />
+        <Tab text="Book a Desk" />
+        <Tab
+            text="Find my Desk"
+        />
+        <Tab
+            text="Book a Meeting Room"
+        />
+        <Tab
+            text="Todo List"
+        />
+        <Tab
+            text="Carpool"
+        />
+        </TabContainer>
+    )
+}
