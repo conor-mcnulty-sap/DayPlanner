2f2d8a2005d593a173d6d18d3f6721968119460b
dylan <xdylar@gmail.com>
2024-04-24 09:43:28 +0100

    h

--- frontend/src/pages/BookDesk.js
+++ frontend/src/pages/BookDesk.js
@@ -1,14 +1,10 @@
 import React from "react";
 import Map from "../components/BookDesk/Map";
-import { Grid } from "@ui5/webcomponents-react";
 
 function BookDesk() {
 
   return (
-    <Grid>
-      <Map/>
-    </Grid>
-    
+      <Map/> 
   )
 }
 export default BookDesk;
