1aac3f8afa3dfb81e6ab7836b24e3a0c865fac33
Eoin Connolly <eoin.connolly.2022@mumail.ie>
2024-04-24 09:52:33 +0100

    Our enemies are innovative and resourceful, and so are we. They never stop thinking about new ways to harm our country and our people, and neither do we.

--- frontend/src/pages/BookDesk.js
+++ frontend/src/pages/BookDesk.js
@@ -4,14 +4,8 @@ import Map from "../components/BookDesk/Map";
 function BookDesk() {
 
   return (
-<<<<<<< HEAD
-    
+
       <Map/>
-    
-    
-=======
-      <Map/> 
->>>>>>> 2f2d8a2005d593a173d6d18d3f6721968119460b
   )
 }
 export default BookDesk;
