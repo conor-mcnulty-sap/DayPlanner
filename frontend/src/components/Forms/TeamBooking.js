78ac37a6e4105b5c2f669cd554fe9f0336f1a8df
conor_mcn55 <conor.mcnulty-cleary@sap.com>
2024-07-01 13:51:29 +0100

    more updates

--- frontend/src/components/Forms/TeamBooking.js
+++ frontend/src/components/Forms/TeamBooking.js
@@ -1,181 +1,226 @@
-import React, { Component } from "react";
-import { sendEmail, getEvents } from "../Tasks/Calendar/GraphFunctions";
+import React, { useState, useEffect } from "react";
 import {
   Form,
   FormGroup,
   FormItem,
   Input,
-  DatePicker,
+  Button,
+  DateRangePicker,
   Select,
   Option,
-  Button
 } from "@ui5/webcomponents-react";
+import { sendEmail, getEvents } from "../Tasks/Calendar/GraphFunctions";
 import config from "../Tasks/Calendar/Config";
 
-export default class TeamBooking extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      emailAddresses: "",
-      subject: "",
-      body: "",
-      events: [],
-      date: "",
-      building: "",
-      floor: "",
-      room: "",
-      displayName: ""
-    };
-  }
+function TeamBooking({
+  selectedDesk,
+  onBuildingChange,
+  onFloorChange,
+  onDateRangeChange,
+}) {
+  const [userId, setUserId] = useState("");
+  const [building, setBuilding] = useState(
+    selectedDesk ? selectedDesk.building : ""
+  );
+  const [floor, setFloor] = useState(selectedDesk ? selectedDesk.floor : "");
+  const [deskId, setDeskId] = useState(selectedDesk ? selectedDesk.deskId : "");
+  const [dateRange, setDateRange] = useState("");
+  const [deskOptions, setDeskOptions] = useState([]);
+  const [emailAddresses, setEmailAddresses] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [accessToken, setAccessToken] = useState("");
 
-  async componentDidMount() {
-    try {
-      // Fetch display name from local storage
-      const storedUserDetails = localStorage.getItem("userDetails");
-      if (storedUserDetails) {
-        const userDetails = JSON.parse(storedUserDetails);
-        this.setState({ displayName: userDetails.displayName });
-      }
+  const today = new Date();
+  const endDate = new Date();
+  endDate.setDate(today.getDate() + 7);
 
-      console.log("Attempting to acquire token silently");
-      const accessToken = await window.msal.acquireTokenSilent({
-        scopes: config.scopes
-      });
-      console.log("Access token acquired", accessToken);
+  const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
 
-      const events = await getEvents(accessToken);
-      console.log("Fetched events", events);
+  const startString = formatDate(today);
+  const endString = formatDate(endDate);
 
-      this.setState({ accessToken, events: events.value });
-    } catch (err) {
-      console.error("Error fetching events", err);
-      if (this.props.showError) {
-        this.props.showError('ERROR', JSON.stringify(err));
-      }
+  const defaultRange = `${startString} - ${endString}`;
+
+  useEffect(() => {
+    if (building) {
+      onBuildingChange(building);
     }
-  }
+  }, [building]);
 
-  handleSendEmail = async () => {
-    const { accessToken, emailAddresses, body, displayName, date } = this.state;
+  useEffect(() => {
+    if (floor) {
+      onFloorChange(floor);
+    }
+  }, [floor]);
 
+  useEffect(() => {
+    const storedUserDetails = localStorage.getItem("userDetails");
+    if (storedUserDetails) {
+      const userDetails = JSON.parse(storedUserDetails);
+      setUserId(userDetails.id);
+      setDisplayName(userDetails.displayName);
+    }
+
+    const fetchAccessTokenAndEvents = async () => {
+      try {
+        console.log("Attempting to acquire token silently");
+        const token = await window.msal.acquireTokenSilent({
+          scopes: config.scopes,
+        });
+        console.log("Access token acquired", token);
+
+        const eventsData = await getEvents(token);
+        console.log("Fetched events", eventsData);
+
+        setAccessToken(token);
+      } catch (err) {
+        console.error("Error fetching events", err);
+      }
+    };
+
+    fetchAccessTokenAndEvents();
+  }, []);
+
+  useEffect(() => {
+    if (floor) {
+      const floorNumber = floor.replace("Floor ", "");
+      fetch(
+        `${process.env.REACT_APP_API_URL}/api/desks/filterbyfloor?floor=${floorNumber}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Desks data:", data);
+          setDeskOptions(data);
+        })
+        .catch((error) => console.error("Error fetching desks:", error));
+    }
+  }, [floor]);
+
+  const handleFloorChange = (event) => {
+    const selectedFloor = event.detail.selectedOption.innerText;
+    setFloor(selectedFloor);
+    if (onFloorChange) {
+      onFloorChange(selectedFloor);
+    }
+  };
+
+  const handleDateRangeChange = (event) => {
+    const [startDate, endDate] = event.detail.value.split(" - ");
+    const formattedStartDate = formatDate(new Date(startDate));
+    const formattedEndDate = formatDate(new Date(endDate));
+    const newDateRange = `${formattedStartDate}-${formattedEndDate}`;
+    setDateRange(newDateRange);
+    if (onDateRangeChange) {
+      onDateRangeChange(newDateRange);
+    }
+  };
+
+  const handleSendEmail = async () => {
     if (!accessToken) {
       console.error("Access token is not available");
       return;
     }
 
-    const emailList = emailAddresses.split(',').map(email => email.trim());
+    const emailList = emailAddresses.split(",").map((email) => email.trim());
 
     const message = {
       message: {
         subject: `Desk Booked For You by ${displayName}`,
         body: {
           contentType: "Text",
-          content: `${displayName} booked a desk for you for ${date}. Please follow this link to verify the booking: http://localhost:3000/verifydesk`
+          content: `${displayName} booked a desk for you for ${dateRange}. Please follow this link to verify the booking: http://localhost:3000/verifydesk`,
         },
-        toRecipients: emailList.map(email => ({ emailAddress: { address: email } }))
+        toRecipients: emailList.map((email) => ({
+          emailAddress: { address: email },
+        })),
       },
-      saveToSentItems: "true"
+      saveToSentItems: "true",
     };
+
+    console.log("Email message to be sent:", message);
 
     try {
       const response = await sendEmail(accessToken, message);
-      console.log('Email sent successfully', response);
+      console.log("Email sent successfully", response);
     } catch (error) {
-      console.error('Error sending email', error);
+      console.error("Error sending email", error);
     }
   };
 
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
-
-  setBuilding = (event) => {
-    this.setState({ building: event.target.value });
-  };
-
-  setFloor = (event) => {
-    this.setState({ floor: event.target.value });
-  };
-
-  setRoom = (event) => {
-    this.setState({ room: event.target.value });
-  };
-
-  render() {
-    const { emailAddresses, date, building, floor, room } = this.state;
-    return (
-      <div
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <Form
+        backgroundDesign="Transparent"
+        columnsL={1}
+        columnsM={1}
+        columnsS={1}
+        columnsXL={1}
+        labelSpanL={4}
+        labelSpanM={2}
+        labelSpanS={12}
+        labelSpanXL={4}
         style={{
-          display: "flex",
-          flexDirection: "column",
           alignItems: "center",
-          paddingLeft:"15rem",
-          width:"50%",
         }}
       >
-        <Form
-          backgroundDesign="Transparent"
-          columnsL={1}
-          columnsM={1}
-          columnsS={1}
-          columnsXL={1}
-          labelSpanL={4}
-          labelSpanM={2}
-          labelSpanS={12}
-          labelSpanXL={4}
-          style={{
-            alignItems: "center",
-          }}
-        >
-          <FormGroup titleText="">
-            <FormItem label="Email Addresses (comma separated)">
-              <Input
-                type="text"
-                name="emailAddresses"
-                value={emailAddresses}
-                onChange={this.handleChange}
-                style={{ width: "100%" }}
-              />
-            </FormItem>
-         
-            <FormItem label="Date">
-              <DatePicker
-                value={date}
-                onChange={(event) => this.setState({ date: event.detail.value })}
-                style={{ width: "100%" }}
-                formatPattern="yyyy-MM-dd"
-              />
-            </FormItem>
-           
-            <FormItem label="Building">
-              <Select
-                onChange={this.setBuilding}
-                selectedKey={building}
-                style={{ width: "100%" }}
-              >
-                <Option>DUB 02</Option>
-                <Option>DUB 05</Option>
-                <Option>GAL</Option>
-              </Select>
-            </FormItem>
-            <FormItem label="Floor">
-              <Select
-                onChange={this.setFloor}
-                selectedKey={floor}
-                style={{ width: "100%" }}
-              >
-                <Option>Floor 1</Option>
-                <Option>Floor 2</Option>
-                <Option>Floor 3</Option>
-              </Select>
-            </FormItem>
-          </FormGroup>
-        </Form>
-        <Button color='primary' onClick={this.handleSendEmail}>
-          Send Email
-        </Button>
-      </div>
-    );
-  }
+        <FormGroup titleText="">
+          <FormItem label="Email Addresses (comma separated)">
+            <Input
+              type="text"
+              name="emailAddresses"
+              value={emailAddresses}
+              onChange={(e) => setEmailAddresses(e.target.value)}
+              style={{ width: "100%" }}
+            />
+          </FormItem>
+          <FormItem label="Dates">
+            <DateRangePicker
+              onChange={handleDateRangeChange}
+              primaryCalendarType="Gregorian"
+              valueState="None"
+              defaultValue={defaultRange}
+              style={{ width: "100%" }}
+            />
+          </FormItem>
+          <FormItem label="Building">
+            <Select
+              onChange={(event) => {
+                const selectedBuilding =
+                  event.detail.selectedOption.dataset.value;
+                setBuilding(selectedBuilding);
+              }}
+              selectedKey={building}
+              style={{ width: "100%" }}
+            >
+              <Option data-value="2">DUB03</Option>
+              <Option data-value="3">DUB05</Option>
+            </Select>
+          </FormItem>
+          <FormItem label="Floor">
+            <Select
+              onChange={handleFloorChange}
+              selectedKey={floor}
+              style={{ width: "100%" }}
+            >
+              <Option>1</Option>
+              <Option>2</Option>
+              <Option>3</Option>
+            </Select>
+          </FormItem>
+        </FormGroup>
+      </Form>
+      <Button color="primary" onClick={handleSendEmail}>
+        Send Email
+      </Button>
+    </div>
+  );
 }
+
+export default TeamBooking;
