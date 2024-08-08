032270815588e4f7ce7c188510cef2e73b1229c3
Sean Zhuang <X00184899@mytudublin.ie>
2024-08-08 13:23:45 +0100

    Changes

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -51,6 +51,31 @@ router.post('/addtask', async (req, res) => {
     let in_taskcolour = req.query.task_colour;
     let in_event_id = req.query.event_id;
 
+    console.log(in_tasktime);
+    console.log(in_taskduration);
+
+
+    // Split the time strings into components
+    let [hours1, minutes1] = in_tasktime.split(':').map(Number);
+    let [hours2, minutes2] = in_taskduration.split(':').map(Number);
+
+    // Add the times together
+    let totalMinutes = minutes1 + minutes2;
+    let totalHours = hours1 + hours2 + Math.floor(totalMinutes / 60);
+
+    // Adjust the total values to fit into proper time format
+    let finalMinutes = totalMinutes % 60;
+    let finalHours = totalHours % 24; // Assuming we are not dealing with more than 24 hours
+
+    // Format the result as HH:MM:SS
+    let newTaskTime = [
+        finalHours.toString().padStart(2, '0'),
+        finalMinutes.toString().padStart(2, '0'),
+    ].join(':');
+
+    console.log("New Task Time: ", newTaskTime);
+
+
     const {data, error} = await supabase
     .from('tasks')
     .insert(
@@ -60,6 +85,7 @@ router.post('/addtask', async (req, res) => {
             description: in_taskdescription,
             date: in_taskdate,
             time: in_tasktime,
+            end_time: newTaskTime,
             duration: in_taskduration,
             colour: in_taskcolour,
             event_id: in_event_id
