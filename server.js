21faffdd9bb171f46e8979655076a883ebef3464
Sean Zhuang <X00184899@mytudublin.ie>
2024-08-14 13:16:49 +0100

    Changes

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const prepareServer = () => {
 
     app.use('/', routes);
  
-    app.listen(5000, () => {
-        console.log('Server is running on port 5000');
+    app.listen(8080, () => {
+        console.log('Server is running on port 8080');
         console.log('Current time: ' + date);
     });
 
